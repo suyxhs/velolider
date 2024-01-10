742cd04a48120374fce647d4c2491597b35b9999
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-01-10 22:08:03 +0300

    best smooth scroll to top

--- components/ScrollToTop/ScrollToTopButton.tsx
+++ components/ScrollToTop/ScrollToTopButton.tsx
@@ -1,18 +1,49 @@
 "use client"
 
-import ScrollToTop from "react-scroll-up"
+import { useEffect, useState } from "react"
 import { ChevronUp } from "lucide-react"
+import clsx from "clsx"
 
+export const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false)
 
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
+  }
 
-export default function ScrollToTopButton() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
   return (
-    <div className="relative z-[300]">
-      <ScrollToTop showUnder={160}>
-        <p className="fixed bottom-4 right-4 rounded-full bg-green-btn p-2 outline-none transition-opacity duration-200 z-40">
-          <ChevronUp />
-        </p>
-      </ScrollToTop>
+    <div className="fixed bottom-2 right-2 z-40">
+      <button
+        type="button"
+        onClick={scrollToTop}
+        className={clsx(
+          isVisible ? 'opacity-100' : 'opacity-0',
+          'fixed bottom-4 right-4 rounded-full bg-green-btn p-2 outline-none transition-opacity duration-200 z-40',
+        )}
+      >
+        <ChevronUp aria-hidden="true" />
+      </button>
     </div>
   )
 }
+
+export default ScrollToTopButton
