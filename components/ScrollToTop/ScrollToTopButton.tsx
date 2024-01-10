f2ad22fa77582fa08e3b23ff60cc85384e505f0b
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-01-10 20:05:42 +0300

    scroll to top update

--- components/ScrollToTop/ScrollToTopButton.tsx
+++ components/ScrollToTop/ScrollToTopButton.tsx
@@ -1,42 +1,18 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import ScrollToTop from "react-scroll-up"
 import { ChevronUp } from "lucide-react"
 
-const ScrollToTopButton = () => {
-  const [isVisible, setIsVisible] = useState(false)
 
-  useEffect(() => {
-    const toggleVisibility = () => {
-      // if the user scrolls down, show the button
-      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false)
-    }
-    // listen for scroll events
-    window.addEventListener("scroll", toggleVisibility)
 
-    // clear the listener on component unmount
-    return () => {
-      window.removeEventListener("scroll", toggleVisibility)
-    }
-  }, [])
-
-  // handles the animation when scrolling to the top
-  const scrollToTop = () => {
-    isVisible &&
-      window.scrollTo({
-        top: 0,
-        behavior: "auto",
-      })
-  }
-
+export default function ScrollToTopButton() {
   return (
-    <button
-      className="fixed bottom-4 right-4 rounded-full bg-green-btn p-2 outline-none transition-opacity duration-200 z-40"
-      onClick={scrollToTop}
-    >
-      <ChevronUp />
-    </button>
+    <div className="relative z-[300]">
+      <ScrollToTop showUnder={160}>
+        <p className="fixed bottom-4 right-4 rounded-full bg-green-btn p-2 outline-none transition-opacity duration-200 z-40">
+          <ChevronUp />
+        </p>
+      </ScrollToTop>
+    </div>
   )
 }
-
-export default ScrollToTopButton
