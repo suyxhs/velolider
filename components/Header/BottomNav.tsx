ae920ac675b87455ba246ce1c8fc35459a932d20
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-01-26 22:23:55 +0300

    update navigation href nav

--- components/Header/BottomNav.tsx
+++ components/Header/BottomNav.tsx
@@ -50,7 +50,7 @@ const NavigationBottom = () => {
         </li>
 
         <li className="w-16 h-16 flex items-center pr-4">
-          <a href="/project" className="flex flex-col text-center">
+          <a href="/projects" className="flex flex-col text-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5">
               <path fill-rule="evenodd" d="M10 3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v9a2 2 0 0 0 2 2h8a2 2 0 0 1-2-2V3ZM4 4h4v2H4V4Zm4 3.5H4V9h4V7.5Zm-4 3h4V12H4v-1.5Z" clip-rule="evenodd" />
               <path d="M13 5h-1.5v6.25a1.25 1.25 0 1 0 2.5 0V6a1 1 0 0 0-1-1Z" />
