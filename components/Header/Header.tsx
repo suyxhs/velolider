be28c4f58499c0b2dd632dd75b3f592d531a39f6
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-02-03 01:00:43 +0300

    open nav dialog update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -81,9 +81,8 @@ export const Header = () => {
 
       <NavbarContent justify="end">
         <NavbarItem className="block md:hidden">
-        <SlideOver Icon={Menu}>
-           <NavigationItems className="flex flex-col items-start gap-8" />
-         </SlideOver>
+
+         <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
         </NavbarItem>
       </NavbarContent>
 
