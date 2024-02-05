957fdf69e8c125ccb2c1d55608f9c164441b48c7
Alexander <104249649+suyxhs@users.noreply.github.com>
2024-02-05 03:36:41 +0300

    color preloader update

--- components/Preloader/index.tsx
+++ components/Preloader/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { opacity, slideUp } from './anim';
 
-const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten tag", "Hallo"]
+const words = ["Привет" ,"Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten tag", "Hallo", "ВелоЛидер"]
 
 export default function Index() {
     const [index, setIndex] = useState(0);
